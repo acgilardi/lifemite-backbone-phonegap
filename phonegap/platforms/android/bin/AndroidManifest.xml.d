73db9bf3835d09e68eb051595fb66ce5ffe57fa1
Anthony Gilardi <admin@appmite.com>
2013-12-15 08:23:43 -0500

    initial: Add initial structure
    Build with phonegap and backbone

---
+++ phonegap/platforms/android/bin/AndroidManifest.xml.d
@@ -0,0 +1,2 @@
+C:\Users\Anthony\Dev\lifemite-backbone-phonegap\phonegap\platforms\android\bin\AndroidManifest.xml : \
+C:\Users\Anthony\Dev\lifemite-backbone-phonegap\phonegap\platforms\android\AndroidManifest.xml \
---
+++ phonegap/platforms/android/bin/HelloWorld-debug-unaligned.apk.d
@@ -0,0 +1,4 @@
+C:\Users\Anthony\Dev\lifemite-backbone-phonegap\phonegap\platforms\android\bin\HelloWorld-debug-unaligned.apk : \
+C:\Users\Anthony\Dev\lifemite-backbone-phonegap\phonegap\platforms\android\bin\HelloWorld.ap_ \
+C:\Users\Anthony\Dev\lifemite-backbone-phonegap\phonegap\platforms\android\bin\classes.dex \
+C:\Users\Anthony\Dev\lifemite-backbone-phonegap\phonegap\platforms\android\libs\cordova-3.1.0.jar \
